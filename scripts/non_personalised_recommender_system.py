b7d16506d1f4597e21034a2b45dd60fdf7b7f8b2
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-07 02:38:54 +0530

    update the script file

--- scripts/non_personalised_recommender_system.py
+++ scripts/non_personalised_recommender_system.py
@@ -60,3 +60,64 @@ pd.isnull(movie_info).sum()
 
 print(pd.isnull(movie_info).sum())
 
+'''
+This dataset contains attributes of 1682 movies. 
+There are 24 columns out of which last 19 columns specify 
+the genre of a particular movie. These are binary columns, 
+i.e., a value of 1 denotes that the movie belongs to that genre, 
+and 0 otherwise.
+
+We have release date missing for only 1 movie in the
+ dataset and rest of the variables do not 
+ have any missing value
+'''
+
+## 3.  Merging Movie information to ratings dataframe 
+'''
+The movie names are contained in a separate file. 
+Let's merge that data with ratings and store it in ratings dataframe.
+The idea is to bring movie title information in ratings dataframe as 
+it would be useful later on
+'''
+ratings = ratings.merge(movie_info[['movie id','movie title']], how='left', left_on = 'movie_id', right_on = 'movie id')
+
+print(ratings.head())
+'''
+Lets also combine movie id and movie title separated by ': ' and store it in a new column named movie
+'''
+
+ratings['movie'] = ratings['movie_id'].map(str) + str(': ') + ratings['movie title'].map(str)
+
+
+print(ratings.columns)
+
+ratings = ratings.drop(['movie id', 'movie title', 'movie_id','unix_timestamp'], axis = 1)
+
+ratings = ratings[['user_id','movie','rating']]
+
+'''
+For using non personalised recommender systems we are only interested in popular movies so we keep movies with atleast 100 ratings in the dataframe and drop the rest
+
+'''
+movie_counts = ratings['movie'].value_counts()
+ratings = ratings[(ratings['movie'].isin(movie_counts[movie_counts >= 100].index))]
+
+'''
+Next, we create a user item matrix using Pandas Pivot Function such that users are in the index and each movie is represented by a separate column**
+- Merge user data with ratings data
+- Create user movie matrix using user ids as rows and movies as columns & name it 'user_movie_matrix'
+'''
+
+n_users = ratings.user_id.unique().shape[0]
+n_items = ratings.movie.unique().shape[0]
+
+print(n_users,n_items)
+
+user_movie_matrix = ratings.pivot(index = 'user_id', columns = 'movie', values = 'rating')
+
+print(user_movie_matrix)
+
+ratings = ratings.merge(users[['user_id','sex']], how = 'left', on = 'user_id')
+ratings = ratings[['user_id','sex','movie','rating']]
+
+
