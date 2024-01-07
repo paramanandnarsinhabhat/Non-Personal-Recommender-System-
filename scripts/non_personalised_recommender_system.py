64d7706bd5d5948e3425f7759c48236b95d0e95a
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-07 12:11:13 +0530

    Update non_personalised_recommender_system.py

--- scripts/non_personalised_recommender_system.py
+++ scripts/non_personalised_recommender_system.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-#1.Reading dataset
+#1.# Load datasets
 users = pd.read_csv('/Users/paramanandbhat/Downloads/Non_personalisedrecommendersystemsinpython-201024-234034 (1)/user_demographics.csv')
 
 #Reading ratings file:
@@ -11,113 +11,79 @@ ratings= pd.read_csv('/Users/paramanandbhat/Downloads/Non_personalisedrecommende
 #Reading items file:
 movie_info = pd.read_csv('/Users/paramanandbhat/Downloads/Non_personalisedrecommendersystemsinpython-201024-234034 (1)/movie_info.csv')
 
-## 2. Basic Exploration 
-### Exploring user data
-# shape of the users data
-print(users.shape)
-# view the users data
-users.head()
+#2.Basic exploration of user data
+print("Users Data Shape:", users.shape)
+print("First 5 rows of Users Data:\n", users.head())
+print("Missing Values in Users Data:\n", pd.isnull(users).sum())
+
+# 3.Basic exploration of ratings data
+print("Ratings Data Shape:", ratings.shape)
+print("First 5 rows of Ratings Data:\n", ratings.head())
+print("Missing Values in Ratings Data:\n", pd.isnull(ratings).sum())
+
+#4 Basic exploration of movie information data
+print("Movie Information Data Shape:", movie_info.shape)
+print("First 5 rows of Movie Information Data:\n", movie_info.head())
+print("Missing Values in Movie Information Data:\n", pd.isnull(movie_info).sum())
+
+# 5.Merge movie information with ratings data
+ratings = ratings.merge(movie_info[['movie id','movie title']], left_on='movie_id', right_on='movie id')
+ratings['movie'] = ratings['movie_id'].astype(str) + ": " + ratings['movie title']
+
+print(ratings['movie'])
+
+# Drop unnecessary columns
+ratings.drop(['movie id', 'movie title', 'movie_id', 'unix_timestamp'], axis=1, inplace=True)
+ratings = ratings[['user_id', 'movie', 'rating']]
+
+print(ratings.describe())
+
+print(ratings.head())
+
+# Filter movies with at least 100 ratings
+movie_counts = ratings['movie'].value_counts()
+ratings = ratings[ratings['movie'].isin(movie_counts.index[movie_counts >= 100])]
+
+# Create user-movie matrix
+# Add the gender information from the users DataFrame to the user_movie_matrix
+users['sex'] = users.set_index('user_id')['sex']
 print(users.head())
 
-pd.isnull(users).sum()
 
-print(pd.isnull(users).sum())
+# Merge user demographics with ratings
+ratings = ratings.merge(users[['user_id', 'sex']], on='user_id')
+ratings = ratings[['user_id', 'sex', 'movie', 'rating']]
 
-'''
-So, we have 943 users in the dataset and each user has 5 features, i.e. user_ID, age, sex, occupation and zip_code. We have no missing values in the user data. Now let’s look at the ratings file.
+# Non-personalized recommender systems using average ratings
+top_avg_ratings = users.mean().sort_values(ascending=False).head(5)
+print("Top 5 Movies by Average Rating:\n", top_avg_ratings)
 
-'''
+# Non-personalized recommender systems using rating count
+top_rating_counts = users.count().sort_values(ascending=False).head(5)
+print("Top 5 Movies by Rating Count:\n", top_rating_counts)
 
-### Exploring ratings data
-# shape of the data
-print(ratings.shape)
-# view the ratings data
-print(ratings.head())
+# Non-personalized recommender systems using count of ratings 4 and above
+ratings_4_above = users[users >= 4].count() / users.count()
+top_ratings_4_above = ratings_4_above.sort_values(ascending=False).head(5)
+print("Top 5 Movies by Count of Ratings 4 and Above:\n", top_ratings_4_above)
 
+# Weak personalization using gender information
+# Calculate the difference in average ratings between male and female users for each movie
+users['sex'] = users['sex']
+# Ensure the matrix only contains numeric values (ratings) and 'sex' column
+user_movie_matrix = users.apply(pd.to_numeric, errors='coerce')
 
-ratings[(ratings['user_id'] == 1)&(ratings['movie_id'] == 100)]
+# Create separate DataFrames for male and female users
+df_m = user_movie_matrix[user_movie_matrix['sex'] == 'M'].drop('sex', axis=1)
+df_f = user_movie_matrix[user_movie_matrix['sex'] == 'F'].drop('sex', axis=1)
 
-print(ratings[(ratings['user_id'] == 1)&(ratings['movie_id'] == 100)])
+# Calculate the mean ratings for each movie by male and female users, handling NaN values appropriately
+mean_ratings_m = df_m.mean(axis=0, skipna=True)
+mean_ratings_f = df_f.mean(axis=0, skipna=True)
 
-pd.isnull(ratings).sum()
+# Calculate the difference in mean ratings between genders
+gender_diff = mean_ratings_f.subtract(mean_ratings_m)
 
-print(pd.isnull(ratings).sum())
-
-'''
-We have 100k ratings for different user and
- movie combinations. Again there are no 
- missing values here.
- Now lets examine the items file.
-'''
-### Exploring Movie Information data
-# shape of the data
-print(movie_info.shape)
-# view the items file
-print(movie_info.head())
-
-# Check missing values in movie information
-pd.isnull(movie_info).sum()
-
-print(pd.isnull(movie_info).sum())
-
-'''
-This dataset contains attributes of 1682 movies. 
-There are 24 columns out of which last 19 columns specify 
-the genre of a particular movie. These are binary columns, 
-i.e., a value of 1 denotes that the movie belongs to that genre, 
-and 0 otherwise.
-
-We have release date missing for only 1 movie in the
- dataset and rest of the variables do not 
- have any missing value
-'''
-
-## 3.  Merging Movie information to ratings dataframe 
-'''
-The movie names are contained in a separate file. 
-Let's merge that data with ratings and store it in ratings dataframe.
-The idea is to bring movie title information in ratings dataframe as 
-it would be useful later on
-'''
-ratings = ratings.merge(movie_info[['movie id','movie title']], how='left', left_on = 'movie_id', right_on = 'movie id')
-
-print(ratings.head())
-'''
-Lets also combine movie id and movie title separated by ': ' and store it in a new column named movie
-'''
-
-ratings['movie'] = ratings['movie_id'].map(str) + str(': ') + ratings['movie title'].map(str)
-
-
-print(ratings.columns)
-
-ratings = ratings.drop(['movie id', 'movie title', 'movie_id','unix_timestamp'], axis = 1)
-
-ratings = ratings[['user_id','movie','rating']]
-
-'''
-For using non personalised recommender systems we are only interested in popular movies so we keep movies with atleast 100 ratings in the dataframe and drop the rest
-
-'''
-movie_counts = ratings['movie'].value_counts()
-ratings = ratings[(ratings['movie'].isin(movie_counts[movie_counts >= 100].index))]
-
-'''
-Next, we create a user item matrix using Pandas Pivot Function such that users are in the index and each movie is represented by a separate column**
-- Merge user data with ratings data
-- Create user movie matrix using user ids as rows and movies as columns & name it 'user_movie_matrix'
-'''
-
-n_users = ratings.user_id.unique().shape[0]
-n_items = ratings.movie.unique().shape[0]
-
-print(n_users,n_items)
-
-user_movie_matrix = ratings.pivot(index = 'user_id', columns = 'movie', values = 'rating')
-
-print(user_movie_matrix)
-
-ratings = ratings.merge(users[['user_id','sex']], how = 'left', on = 'user_id')
-ratings = ratings[['user_id','sex','movie','rating']]
-
-
+# Sort the differences and print the top 5 movies with the largest difference
+top_gender_diff = gender_diff.sort_values(ascending=False).head(5)
+print("Top 5 Movies with the Largest Difference in Average Ratings Between Genders:\n", top_gender_diff)
